c0626e41dbe1d84df713815a3189a671e349ae14
azharfahri <fahrikatepp@gmail.com>
2025-06-18 13:21:43 +0700

    Belajar route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\LatihanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//routing dasar
+Route::get('/sample',function(){
+    return "halo";
+});
+Route::get('/sample2', function(){
+    return view('sample2');
+});
+
+//routing, controller & view dasar
+Route::get('/sample3', [LatihanController::class, 'index']);
